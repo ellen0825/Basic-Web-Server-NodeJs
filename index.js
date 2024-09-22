772cec6b8e0a850277a52756f8fb22775d8c58e0
ellen0825 <ellenmartinelli0825@outlook.com>
2024-09-22 14:22:22 +0700

    prepare for route /data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const url = require('url')
 
 const requestListener = (req, res) => {
     const { url } = req
@@ -7,8 +8,12 @@ const requestListener = (req, res) => {
         case '/':
             requestListenerOnRoot(req, res)
             break;
-        case '/profile':
-            requestListenerOnProfile(req, res)
+        case '/data':
+            requestListenerOnData(req, res)
+            break;
+        case '/data/1':
+            res.setHeader('Content-Type', 'text/html')
+            res.end('Data 1')
             break;
         default:
             break;
@@ -21,21 +26,23 @@ function requestListenerOnRoot(req, res) {
     switch (method) {
         case 'GET':
             res.setHeader('Content-Type', 'text/html')
-            res.end('Welcome! You success the website!')
-            break;
-        case 'POST':
+            const stringResponse = `
+                <h1>Welcome! You have successfully accessed the website!</h1>
+                <p>Current position: root</p>
+            `
+            res.end(stringResponse)
             break;
         default:
             break;
     }
 }
 
-function requestListenerOnProfile(req, res) {
+function requestListenerOnData(req, res) {
     const { method } = req
 
     switch (method) {
         case 'GET':
-            
+            res.end('You are in the data page')
             break;
         case 'POST':
             break;
